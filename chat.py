549fc42f86cc144ed12525bd971490048b6621a0
Marvel paul <djmarvel@yahoo.com>
2024-05-25 14:24:49 -0700

    home and base template

--- chat.py
+++ chat.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret123"
 socketio = SocketIO(app)
 
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
