a77e1dbe53194d79662267926f9af4b1220e9ca3
Marvel paul <djmarvel@yahoo.com>
2024-05-26 02:56:27 -0700

    home and base template

--- chat.py
+++ chat.py
@@ -80,5 +80,19 @@ def connect(auth):
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
 
+#leaving the room
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] <= 0:
+            del rooms[room]
+
+    send({"name": name, "message": "has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
