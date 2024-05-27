73ed47598482225de7cbc1c4b23c31713d482566
Marvel paul <djmarvel@yahoo.com>
2024-05-27 04:25:57 -0700

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -23,15 +23,26 @@ class BaseModel:
         to_dict(self): returns the dictionary values of the instance obj
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Public instance artributes initialization
         after creation
         """
-
+        
+        date_time_format = '%Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == __class__:
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value,
+                            date_time_format))
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """
@@ -62,3 +73,17 @@ class BaseModel:
         inst_dict["created_at"] = self.created_at.isoformat()
         inst_dict["updated_at"] = self.updated_at.isoformat()
         return inst_dict
+
+if __name__ == "__main__":
+    user = BaseModel()
+    user.name = "marvel"
+    print("-------srt----")
+    print(user)
+    print("-----strend----")
+    print("-----update time----")
+    user.save()
+    print(user)
+    print("---------------------------")
+    print("------------------------TO DICT -----------------------------")
+    user_json = user.to_dict()
+    print(user_json)
